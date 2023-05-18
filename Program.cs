62afb278b9a78d1fcda84932d9afca42320d3bad
Nathan Schultz <milisha98@gmail.com>
2023-05-18 15:19:19 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Mongo_Docker;
+using MongoDB.Driver;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Docker Demo - Lets use MongoDB");
+
+            // List People
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("MySuperiorDB");
+            var collection = database.GetCollection<People>("People");
+
+            var people = await collection.Find<People>(_ => true).ToListAsync();
+            Console.WriteLine($"Found {people.Count} records");
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Id}: {person.Name}");
+            }
+
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+
+    }
+}
